78d6f476f0be0d1c6d7b23f6eacc2a9b40225bad
James Blake <142352216+blakejam371@users.noreply.github.com>
2024-02-20 20:16:32 +0000

    Add all possible outcomes to playRound function

--- rps.js
+++ rps.js
@@ -7,7 +7,7 @@ function computerSelection(options) {
 }
 
 // create a function to play a round of Rock Paper Scissors
-// two inputs player and computer choice
+// two inputs player and computer selection
 function playRound(playerSelection, computerSelection) {
   console.log(`Computer Selection: ${computerSelection}`);
   if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Paper") {
@@ -16,7 +16,20 @@ function playRound(playerSelection, computerSelection) {
     console.log("It's a draw");
   } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Scissors") {
     console.log("You Win, Rock beats Scissors");
+  } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Paper") {
+    console.log("It's a draw")  
+  } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Rock") {
+    console.log("You Win, Paper beats Rock");
+  } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Scissors") {
+    console.log("You Lose, Scissors beat Paper");
+  } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Paper") {
+    console.log("You Win, Scissors beat Paper");
+  } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Rock") {
+    console.log("You Lose, Rock beats Scissors");
+  } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Scissors") {
+    console.log("It's a draw");
+  } else {
+    return "Invalid result"
   }
 }
-
-playRound("rock", computerSelection(options));
+playRound("scissors", computerSelection(options));
