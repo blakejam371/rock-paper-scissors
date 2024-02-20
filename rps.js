caff25f16a5fdbe0b0559d393ba80f28bb45ab6d
James Blake <142352216+blakejam371@users.noreply.github.com>
2024-02-20 20:33:15 +0000

    add initial playGame function using a for loop

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
 // declare options
-let options = ["Rock", "Paper", "Scissors"];
+const options = ["Rock", "Paper", "Scissors"];
 
 // create function to randomly get computer choice
 function computerSelection(options) {
@@ -9,27 +9,34 @@ function computerSelection(options) {
 // create a function to play a round of Rock Paper Scissors
 // two inputs player and computer selection
 function playRound(playerSelection, computerSelection) {
-  console.log(`Computer Selection: ${computerSelection}`);
   if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Paper") {
-    console.log("You Lose! Paper beats Rock");
+    return("You Lose! Paper beats Rock");
   } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Rock") {
-    console.log("It's a draw");
+    return("It's a draw");
   } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Scissors") {
-    console.log("You Win, Rock beats Scissors");
+    return("You Win, Rock beats Scissors");
   } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Paper") {
-    console.log("It's a draw")  
+    return("It's a draw")  
   } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Rock") {
-    console.log("You Win, Paper beats Rock");
+    return("You Win, Paper beats Rock");
   } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Scissors") {
-    console.log("You Lose, Scissors beat Paper");
+    return("You Lose, Scissors beat Paper");
   } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Paper") {
-    console.log("You Win, Scissors beat Paper");
+    return("You Win, Scissors beat Paper");
   } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Rock") {
-    console.log("You Lose, Rock beats Scissors");
+    return("You Lose, Rock beats Scissors");
   } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Scissors") {
-    console.log("It's a draw");
+    return("It's a draw");
   } else {
-    return "Invalid result"
+    return "Invalid result";
   }
 }
-playRound("scissors", computerSelection(options));
+
+// create playGame function - loops 'rounds' number of times
+function playGame(rounds) {
+  for (let i = 0; i < rounds; i++) {
+    console.log(playRound("Rock", computerSelection(options)));
+  }
+}
+
+playGame(5);
